5b204bead8b07c2edcce05c1410d23f1ecafe813
RodRibDev <rodrigo_r_ribeiro@estudante.sesisenai.org.br>
2024-04-19 07:53:54 -0300

    Execução do exercício 01 S11

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -81,7 +81,11 @@ routes.get('/cursos', async (req, res) => {
     if(req.query.nome)  {
         params = {...params, nome: req.query.nome}
     }
-    
+
+    if(req.query.duracao_horas)  {
+        params = {...params, duracao_horas: req.query.duracao_horas}
+    }
+
     const cursos = await Curso.findAll({
         where: params
     })
